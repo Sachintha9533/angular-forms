92d5444a80f5ebb3e64e5b4c14c28bb560f458e8
Sachintha <sachinthaslakshan@gmail.com>
2022-12-11 08:57:03 +0530

    first commit

--- src/app/angular-forms/angular-forms.component.ts
+++ src/app/angular-forms/angular-forms.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-angular-forms',
@@ -6,6 +6,19 @@ import { Component } from "@angular/core";
   styleUrls: ['./angular-forms.component.css'],
 })
 export class AngularFormsComponent {
-  models = ['Lambogini','Ferrari','BMW'];
-  colors = ['Red','Black','Blue'];
+  
+  models = ['Lambogini', 'Ferrari', 'BMW'];
+  colors = ['Red', 'Black', 'Blue'];
+Form: any;
 }
+
+onsubmit() {
+console.warn(this.form.value);
+}
+
+
+
+
+
+
+
